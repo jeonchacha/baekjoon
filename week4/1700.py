33980982e7a8ea6c71012da46a9c158c1c808cc0
jeonseyeong <jsy9831@gmail.com>
2025-08-06 00:41:20 +0900

    1700 수정

--- week4/1700.py
+++ week4/1700.py
@@ -2,21 +2,39 @@
 import sys
 input = sys.stdin.readline
 N, K = map(int, input().split())
-item = list(input().split())
+usage_order = input().split()
+# N, K = 2, 7
+# usage_order = ['2', '3', '2', '3', '1', '2', '7']
+def min_unplugs(num_slots, usage_order):
+    plugs = []
+    unplug_count = 0
 
-def schedule(n, item):
-    a = []
-    for i in range(n):
-        a.append(item[i])
-    count = 0
-    for i in item[n+1:]:
-        for j in a:
-            if i == j:
-                break
-        else:
-            a[-1] = i
-            count += 1
+    for current_index in range(len(usage_order)):
+        current_device = usage_order[current_index]
+        if current_device in plugs:
+            continue
+        
+        if len(plugs) < num_slots:
+            plugs.append(current_device)
+            continue
+        
+        latest_use = -1
+        device_to_unplug = -1
 
-    return count
+        for plugged_device in plugs:
+            next_use = float('inf')
+            for future_index in range(current_index + 1, len(usage_order)):
+                if usage_order[future_index] == plugged_device:
+                    next_use = future_index
+                    break
 
-print(schedule(N, item))
+            if next_use > latest_use:
+                latest_use = next_use
+                device_to_unplug = plugged_device
+
+        plugs.remove(device_to_unplug)
+        plugs.append(current_device)
+        unplug_count += 1            
+    return unplug_count
+
+print(min_unplugs(N, usage_order))
